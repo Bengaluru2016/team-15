cc584f02f0a11c3f44a7cad961a0e399c25a60ea
Ramyaponnada <ramya_sarma@rediffmail.com>
2016-07-10 10:05:00 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+</head>
+<body>
+<form action="final.php" method="POST">
+<table>
+<tr>
+<div class="form-group">
+<td>ID:</td><td><input type="text" name="id"></input></td></br>
+</div>
+</tr>
+<tr>
+<div class="form-group">
+<td>Name:</td><td><input type="text" name="name"></input></td></br>
+</div>
+</tr>
+<tr>
+ <div class="form-group">
+<td>Score:</td><td><input type="text" name="score"></input></td></br>
+</div>
+</tr>
+</div>
+</br>
+<tr>
+<div class="form-group">
+<td>User_type:</td><td><input type="text" name="user type"></input></td></br>
+</div>
+</tr>
+</tr></br>
+<tr><td><input type="submit" name="submit" value="submit"></input></td></tr></br>
+</table>
+</form>
+</body>
+</html>
